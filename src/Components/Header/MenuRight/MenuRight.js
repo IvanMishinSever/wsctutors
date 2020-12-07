a909c376619d14f3a12d516a4540c5d862f39c4f
Big ML <m@y.ru>
2020-12-07 20:53:25 +0300

    add MenuRight

---
+++ src/Components/Header/MenuRight/MenuRight.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MenuRight.css';
+
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <div className="MenuRight">
+                <p>значок1</p>
+                <p>значок2</p>
+            </div>
+        )
+    }
+}
