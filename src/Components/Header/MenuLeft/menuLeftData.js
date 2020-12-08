6b27223aed73f6ca766d1819e99ee9e0dddae389
Mark Leb <wscad@yandex.ru>
2020-12-08 06:25:02 +0300

    add menuLeftData

---
+++ src/Components/Header/MenuLeft/menuLeftData.js
@@ -0,0 +1,37 @@
+const menuLeftData = [
+    {
+        name: 'Курсы',
+        subName: [
+            {
+            name: 'Сопротивление материалов'
+            },
+            {
+            name: 'Инженерная геология'
+            },
+            {
+            name: 'Основания зданий и сооружений'
+            },
+    ]
+    },
+    {
+        name: 'Тесты',
+        subName: [
+            {
+            name: 'Сопротивление материалов'
+            },
+            {
+            name: 'Инженерная геология'
+            },
+            {
+            name: 'Основания зданий и сооружений'
+            },
+    ] 
+    },
+    {name: 'Цены'}
+];
+/*
+for calling data yoy need -
+menuleftData.[i].name
+menuleftData.[i].subName.[j].name
+*/
+export default menuLeftData;
