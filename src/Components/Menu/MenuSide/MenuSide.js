7fb362adcb129492c61ac2d372da0d4b4022e7b5
Mark Leb <wscad@yandex.ru>
2020-12-17 06:38:23 +0300

    add menuside state and folder utils

--- src/Components/Menu/MenuSide/MenuSide.js
+++ src/Components/Menu/MenuSide/MenuSide.js
@@ -3,15 +3,31 @@ import './MenuSide.css';
 
 
 export default class MenuSide extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            categoryView: false,
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    
+    handleClick() {
+        this.setState({
+            categoryView: true
+        })
+        console.log(this.state.categoryView);
+        
+
+    }
     render() {
         return (
             <div className="MenuSide">
                 <ul>
                     <li>
-                        <div><a href="index.html">Курсы</a></div>
+                        <div><button  onClick={this.handleClick}>Курсы</button></div>
                     </li>
                     <li>
-                        <div><a href="index.html">Тесты</a></div>
+                        <div><a href="index.html" onClick={this.handleClick}>Тесты</a></div>
                         
                     </li>
                     
