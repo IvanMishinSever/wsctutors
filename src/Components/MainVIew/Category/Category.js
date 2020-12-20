77f49fb78158aabffcd036003f0462f11537a901
Big ML <m@y.ru>
2020-12-20 19:17:26 +0300

    change return null

--- src/Components/MainVIew/Category/Category.js
+++ src/Components/MainVIew/Category/Category.js
@@ -7,14 +7,18 @@ export default class Category extends React.Component {
     render() {
         let showInfo = this.props.categoryView;
        // let showInfo = true;
+       if (showInfo) {
         return (
-            <div style={{display: showInfo ? "block" : "none"}} className="Category">
+            <div  className="Category">
                 <ul>
                     <li><button>Инженерная геология</button></li>
                     <li><button>Строительная механика</button></li>
                     <li><button>Железобетоные конструкции</button></li>
                 </ul>
             </div>
-        )
+       
+
+            )
+        }  else return null;
     }
 }
