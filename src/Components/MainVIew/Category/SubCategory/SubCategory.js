b542cae34a2f61840d58c611c49e1792872ffeba
Mark Leb <wscad@yandex.ru>
2020-12-27 15:01:08 +0300

    add handler lists

--- src/Components/MainVIew/Category/SubCategory/SubCategory.js
+++ src/Components/MainVIew/Category/SubCategory/SubCategory.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './SubCategory.css';
-
+import FinalLists from './FinalLists/FinalLists';
 
  //ADD SUBCATEGORY
  const courseSubCategory_1 = ['Грунты_1','СП','ГОСТЫ'];
@@ -8,7 +8,7 @@ import './SubCategory.css';
  const testSubCategory_1 = ['Грунты-test_1','СП-test','ГОСТ-test','R-test'];
  const testSubCategory_2 = ['Грунты-test_2','СП-test','ГОСТ-test','R-test'];
  const chooseSubCategory = (idCat,idSub) => {
-     if (idCat === "1" && idSub == "1") {
+     if (idCat === "1" && idSub === "1") {
          return courseSubCategory_1;
      } else if (idCat === "1" && idSub === "2") {
          return courseSubCategory_2;
@@ -23,11 +23,26 @@ import './SubCategory.css';
  
  }
 export default class SubCategory extends React.Component  {
+    constructor(props) {
+        super(props);
+        this.state = {
+            finalListsView: false,
+            idFinalLists: "1"
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(e) {
+ 
+        this.setState({
+            finalListsView: true,
+            idFinalLists: e.target.value
+        })
+    }
 
     render() {
         let showInfo = this.props.subcategoryView;
        // let showInfo = true;
-       //RECEIVE SUBCATEGORY and CATEGORY
+       //RECEIVE SUBCATEGORY and CATEGORY 
        let idSubCategory = this.props.idSubCategory;
        //let idSubCategory = "2";
        let idCategory = this.props.idCategory;
@@ -37,11 +52,12 @@ export default class SubCategory extends React.Component  {
         return (
             <div  className="SubCategory">
                 <ul>
-                    <li><button>{nameSubCategory[0]}</button></li>
-                    <li><button>{nameSubCategory[1]}</button></li>
+                    <li><button onClick={this.handleClick} value="1">{nameSubCategory[0]}</button></li>
+                    <li><button onClick={this.handleClick} value="2">{nameSubCategory[1]}</button></li>
                     <li><button>{nameSubCategory[2]}</button></li>
                     <li><button>{nameSubCategory[3]}</button></li>
                 </ul>
+                <FinalLists finalListsView={this.state.finalListsView}  idFinalLists={this.state.idFinalLists} idSubCategory={this.props.idSubCategory} idCategory={this.props.idCategory}/>
             </div>
        
 
