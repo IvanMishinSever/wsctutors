0c05aa40de6117342a1bf582ef469c6133ac44d3
Mark Leb <wscad@yandex.ru>
2020-12-27 12:16:32 +0300

    add sub category id

--- src/Components/MainVIew/Category/SubCategory/SubCategory.js
+++ src/Components/MainVIew/Category/SubCategory/SubCategory.js
@@ -2,18 +2,36 @@ import React from 'react';
 import './SubCategory.css';
 
 
+ //ADD SUBCATEGORY
+ const courseSubCategory = ['Грунты','СП','ГОСТЫ'];
+ const testSubCategory = ['Грунты-test','СП-test','ГОСТ-test'];
+ const chooseSubCategory = (id) => {
+     if (id === "1") {
+         return courseSubCategory;
+     } else if (id === "2") {
+         return testSubCategory;
+     } else {
+        // alert("change number of the category id!");
+        return null;
+     }
+ 
+ }
 export default class SubCategory extends React.Component  {
 
     render() {
         let showInfo = this.props.subcategoryView;
        // let showInfo = true;
+       //RECEIVE SUBCATEGORY
+      // let idSubCategory = this.props.idSubCategory;
+       let idSubCategory = "2";
+       let nameSubCategory = chooseSubCategory(idSubCategory);
        if (showInfo) {
         return (
             <div  className="SubCategory">
                 <ul>
-                    <li><button>Грунты</button></li>
-                    <li><button>СП</button></li>
-                    <li><button>ГОСТ</button></li>
+                    <li><button>{nameSubCategory[0]}</button></li>
+                    <li><button>{nameSubCategory[1]}</button></li>
+                    <li><button>{nameSubCategory[2]}</button></li>
                 </ul>
             </div>
        
