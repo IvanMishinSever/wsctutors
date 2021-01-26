f0c9584f45754de27000deb286dfa4c8b1f7ed57
Mark Leb <wscad@yandex.ru>
2021-01-26 06:44:06 +0300

    add plus or minus

--- src/Components/MainVIew/Nodes/Nodes.js
+++ src/Components/MainVIew/Nodes/Nodes.js
@@ -5,7 +5,8 @@ export default class Nodes extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            expanded: false
+            expanded: false,
+            nodesExist: false
         }
         this.toggle = this.toggle.bind(this);
         this.showNodes = this.showNodes.bind(this);
@@ -18,18 +19,45 @@ export default class Nodes extends React.Component {
             expanded: expanded
         })
     }
+                // change state!!!!!!!!!!!!!
+    checkNodes() {
+        if (this.props.element.nodes) {
+        this.setState({
+            nodesExist: true
+        })
+        }
+    }            
+                
     //SHOW NODES
     showNodes() {
         let arrayNodes = [];
         console.log(arrayNodes);
         if (this.props.element.nodes ) {
+
+
+           //print nodes 
             this.props.element.nodes.map((element, index) => {
                 arrayNodes.push(<Nodes key={index} element={element} />);
             });
             }
             return arrayNodes;
         }
-        
+     renderButton() {
+         const sign = this.props.element.nodes;
+         //console.log(sign);   
+         if (sign) {
+         return(
+            <button
+                        type="button"
+                        onClick={this.toggle}
+                    >+</button>
+         ) 
+         } else {
+            return(
+                <button> - </button>
+             )   
+         }
+     }   
     
 
     //RENDER NODE
@@ -38,10 +66,7 @@ export default class Nodes extends React.Component {
         return (
             <div className="Node-container">
                 <div className="Node">
-                    <button
-                        type="button"
-                        onClick={this.toggle}
-                    >+</button>
+                    {this.renderButton()}
                     <h6>{this.props.element.label}
                     <small>{this.props.element.id}</small>
                     </h6>
