4ec3de74341a4abbdb8cc8ba74b74f349a890bde
Mark Leb <wscad@yandex.ru>
2021-01-28 06:27:04 +0300

    add data for quiz

---
+++ src/Components/MainVIew/Quiz/quizData.js
@@ -0,0 +1,163 @@
+Quiz = [
+    {
+        id:"1",
+        question: {
+            text:"Минимальная длина анкеровки составляет -",
+            img:""
+        },
+        answers:[{
+            id:"1",
+            label:true,
+            text:"200мм",
+            img:""
+        },{
+            id:"2",
+            label:false,
+            text:"300мм",
+            img:""
+        },{
+            id:"3",
+            label:false,
+            text:"100мм",
+            img:""
+        }]
+    },
+    {
+        id:"2",
+        question: {
+            text:"Минимальная длина нахлеста арматуры составляет -",
+            img:""
+        },
+        answers:[{
+            id:"1",
+            label:true,
+            text:"200мм",
+            img:""
+        },{
+            id:"2",
+            label:false,
+            text:"300мм",
+            img:""
+        },{
+            id:"3",
+            label:false,
+            text:"100мм",
+            img:""
+        }]
+    },
+    {
+        id:"3",
+        question: {
+            text:"Минимальный процент армирования для столбчатого фундамента составляет -",
+            img:""
+        },
+        answers:[{
+            id:"1",
+            label:true,
+            text:"в зависимости от вида -для подошвы-не нормировано",
+            img:""
+        },{
+            id:"2",
+            label:false,
+            text:"1%",
+            img:""
+        },{
+            id:"3",
+            label:false,
+            text:"2%",
+            img:""
+        }]
+    },
+    {
+        id:"4",
+        question: {
+            text:"Минимальный процент армирования для центрально-сжатой колонны составляет -",
+            img:""
+        },
+        answers:[{
+            id:"1",
+            label:true,
+            text:"1%",
+            img:""
+        },{
+            id:"2",
+            label:false,
+            text:"5%",
+            img:""
+        },{
+            id:"3",
+            label:false,
+            text:"2%",
+            img:""
+        }]
+    },
+    {
+        id:"5",
+        question: {
+            text:"Минимальный процент армирования для сжато-изогнутой колонны составляет -",
+            img:""
+        },
+        answers:[{
+            id:"1",
+            label:true,
+            text:"см. СП",
+            img:""
+        },{
+            id:"2",
+            label:false,
+            text:"1%",
+            img:""
+        },{
+            id:"3",
+            label:false,
+            text:"2%",
+            img:""
+        }]
+    },
+    {
+        id:"6",
+        question: {
+            text:"Максимальный шаг поперечных стержней в зоне нахлеста колонны составляет -",
+            img:""
+        },
+        answers:[{
+            id:"1",
+            label:true,
+            text:"см. сп",
+            img:""
+        },{
+            id:"2",
+            label:false,
+            text:"300мм",
+            img:""
+        },{
+            id:"3",
+            label:false,
+            text:"50мм",
+            img:""
+        }]
+    },
+    {
+        id:"7",
+        question: {
+            text:"Максимальное количество растянутых стержней, которое можно обхватить одним хомутом составляет -",
+            img:""
+        },
+        answers:[{
+            id:"1",
+            label:true,
+            text:"4",
+            img:""
+        },{
+            id:"2",
+            label:false,
+            text:"5",
+            img:""
+        },{
+            id:"3",
+            label:false,
+            text:"3",
+            img:""
+        }]
+    }
+]
