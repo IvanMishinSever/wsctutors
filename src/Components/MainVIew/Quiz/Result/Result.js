4627fb641bc57a516248d20e5a1ae29943e25511
Mark Leb <wscad@yandex.ru>
2021-02-08 05:48:20 +0300

    add new comp result

---
+++ src/Components/MainVIew/Quiz/Result/Result.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Result(props) {
+    return (
+        <div className="Result">
+            Вы ответили правильно на 1 вопросов.
+            Ваш результат <strong>{props.quizResult}</strong>
+        </div>
+    )
+}
