b47c1cae182563a18d2b86b33e3f1fb128fdb7a2
Mark Leb <wscad@yandex.ru>
2021-04-02 06:06:13 +0300

    add admin in right menu

--- src/Components/Header/MenuRight/MenuRight.js
+++ src/Components/Header/MenuRight/MenuRight.js
@@ -6,6 +6,7 @@ export default class MenuRight extends React.Component {
     render() {
         return (
             <div className="MenuRight">
+                <p id="admin">Admin</p>
                 <p>Найти</p>
                 <p>Личный кабинет</p>
             </div>
