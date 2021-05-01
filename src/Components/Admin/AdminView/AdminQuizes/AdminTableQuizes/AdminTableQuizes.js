20c5b8ee846485b8869f898a6e3b24b446f8969c
Mark Leb <wscad@yandex.ru>
2021-05-01 23:12:04 +0300

    after merge

--- src/Components/Admin/AdminView/AdminQuizes/AdminTableQuizes/AdminTableQuizes.js
+++ src/Components/Admin/AdminView/AdminQuizes/AdminTableQuizes/AdminTableQuizes.js
@@ -31,45 +31,50 @@ export default class AdminTableQuizes extends React.Component {
         super(props);
         this.state = {
             dataTable: [],
+            isFetching: true,
+            error: null
         }
+        this.getAllUsers = this.getAllUsers.bind(this);
     }
     
-    async getAllUsers() {
-        const url = "http://localhost:4001/api/quizes/";
-        const urlToFetch = url;
-        try {
-            const response = await fetch(urlToFetch);
-            console.log(response);
-            if (response.ok) {
-                const jsonResponse = await response.json();
-                printQuizesCategory(jsonResponse);
-               // this.addDataTable(jsonResponse);
-               const items = [];
-               items.push(jsonResponse);
-               this.setState({
-                dataTable: items,
-                });
-                console.log(jsonResponse);
-            }
+async getAllUsers() {
+    const url = "http://localhost:4001/api/quizes/";
+    const urlToFetch = url;
+    try {
+        const response = await fetch(urlToFetch);
+        console.log(response);
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            this.setState({
+                dataTable: jsonResponse,
+                isFetching: false,
+             
+            })   
+           // printQuizesCategory(jsonResponse);
+            
+           
+            
+           // console.log(jsonResponse);
         }
-        catch (error) {
-            console.log(error);
-            console.log('SOMETHING WRONG!!!')
-        }
-    
     }
-/*
- addDataTable(jsonResponse) {
-
-    this.setState({
-         dataTable: jsonResponse,
-     });
-     //console.log("dddddddd");
-
- }
-*/
-
+    catch (error) {
+        this.setState({
+            isFetching: false,
+            error:error.message
+        })
+        console.log(error);
+        console.log('SOMETHING WRONG!!!')
+       
+    }
+    
+}
     render() {
+    /*
+    // НЕ ПОНЯТНО ЗАЧЕМ ISFETCHING  
+    const  {dataTable, isFetching, error} = this.state;  
+    if (isFetching) return <div>...Loading</div>;
+    if (error) return <div>{`Error: ${error}`}</div>;
+*/
         return (
             
             <div className="AdminTableQuizes">
