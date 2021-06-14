5f7cc1db3814306fffebe2ea088b158343eb901b
Big ML <m@y.ru>
2021-06-14 18:38:16 +0300

    add getinitialstate after menuside button

--- src/Components/Menu/MenuSide/MenuSide.js
+++ src/Components/Menu/MenuSide/MenuSide.js
@@ -1,90 +1,31 @@
 import React from 'react';
 import './MenuSide.css';
 import {showFirstContentView, addMenuNodes, loadMenuNodes } from "../MenuSide/menuSideSlice";
-/*
-export default class MenuSide extends React.Component {
-    constructor(props) {
-        super(props);
-       
-        this.handleClick = this.handleClick.bind(this);
-    }
-    
-    handleClick(e) {
-        const id = e.target.value;
-        //const id = 1;
-        this.props.chooseCategory(id);
-       // console.log(id);
+import { getInitialState } from '../../MainVIew/Quiz/quizSlice.js';
 
-      }
-      onShowFirstContent =() => {
-        console.log(showFirstContentView);
-          this.props.dispatch(showFirstContentView());
-      }
-    
-
-
-//<ul onClick={this.props.handleClick}>
-    render() {
-        return (
-            <div className="MenuSide">
-                <ul >
-                    <li>
-                        <button onClick={this.handleClick} value="0"> Курсы </button>
-                    </li>
-                    <li>
-                        <button onClick={this.handleClick} value="1"> Тесты </button>
-                        <button onClick={this.onShowFirstContent} value="2">Example</button>
-                    </li>
-                    
-                    
-                </ul>
-                <form onClick={this.handleClick}>
-                    <input type="radio" name="src" value="1" /> fast
-                    <input type="radio" name="src" value="2" /> slow
-                    <input type="radio" name="src" value="3" /> cute
-                    <input type="radio" name="src" value="4" /> eek
-                </form>
-            </div>
-        )
-    }
-}
-*/
 const MenuSide = (props) => {
  const { dispatch } = props;
 
-    const handleClick = (e) => {
-        const id = e.target.value;
-        props.chooseCategory(id);
- 
-      }
+
 
 // SET ID CATEGORY AND FETCH, если курсы то другая функция
       const onShowCategoryRedux = (e) => {
         const id = e.target.value;
         if (id === 2)  {
             dispatch(loadMenuNodes());
+            dispatch(getInitialState());
         } else {
             dispatch(loadMenuNodes());
+            dispatch(getInitialState());
         }
-        
-        //dispatch(addMenuNodes(id));
-       // console.log();
-        
-        
-        
-      }
-/*
-    const onShowFirstContent =() => {
-        console.log(showFirstContentView);
-        dispatch(showFirstContentView());
+  
       }
 
-*/
         return (
             <div className="MenuSide">
                 <ul >
                     <li>
-                        <button onClick={handleClick} value="0"> Курсы </button>
+                        <button onClick={onShowCategoryRedux} value="0"> Курсы </button>
                     </li>
                     <li>
                         <button onClick={onShowCategoryRedux} value="1"> Тесты </button>
@@ -93,12 +34,7 @@ const MenuSide = (props) => {
                     
                     
                 </ul>
-               <form onClick={handleClick}>
-                   <input type="radio" name="src" value="1" /> fast
-                    <input type="radio" name="src" value="2" /> slow
-                    <input type="radio" name="src" value="3" /> cute
-                    <input type="radio" name="src" value="4" /> eek
-                </form>
+               
             </div>
         )
     
